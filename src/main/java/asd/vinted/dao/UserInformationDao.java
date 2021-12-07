2c85a6f7ece5206a54b8d60d0923e63f80f07402
Gebreyowhans <gebreyohans2005@gmail.com>
2021-12-07 11:36:54 +0100

    Profile area Backend started

---
+++ src/main/java/asd/vinted/dao/UserInformationDao.java
@@ -0,0 +1,14 @@
+package asd.vinted.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import asd.vinted.entity.User;
+import asd.vinted.entity.UserInformation;
+
+public interface UserInformationDao extends JpaRepository<UserInformation,Long> {
+
+    UserInformation findById(long id);
+    UserInformation findByUserId(long userId);
+    void addUserInformation(UserInformation user);
+    void update(UserInformation user);
+}
