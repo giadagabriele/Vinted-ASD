d171c1cd050b182f52852e40a44b82c0be87bf5e
Beatrice26 <mottabea@gmail.com>
2021-12-17 19:03:46 +0100

    update front-end personalization

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -44,7 +44,7 @@ export class UserService {
       console.log(user);
 
       if (user != null) {
-        user.id=0;
+        user.id ='';
        
         this.httpClient.post('http://localhost:8080/user/loginGoogle',user).subscribe((res) => {
        console.log(res);
