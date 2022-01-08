332abb552bfc76048a364b89fdff79a36e1f1fab
Tesfay Gebremeskel <tesfayegec@gmail.com>
2022-01-08 17:59:58 +0100

    product fetch by category fixed, message and favorite with repective user fixed

--- src/main/java/asd/vinted/data/service/ProductService.java
+++ src/main/java/asd/vinted/data/service/ProductService.java
@@ -5,7 +5,7 @@ import asd.vinted.data.dto.ProductDto;
 import asd.vinted.data.entity.Product;
 
 public interface ProductService {
-
+   List<ProductDto> getProductByCategory(String category);
     List<ProductDto> getAllProducts();
     ProductDto getProduct(Long id);
     ProductDto addProduct(ProductDto product);
