9720f6ea37f7064a9844baeff56a548dac044bc0
Abdelzaher2100 <56964365+Abdelzaher2100@users.noreply.github.com>
2022-01-11 00:04:24 +0100

    City

--- src/main/java/asd/vinted/data/service/impl/CityServiceImpl.java
+++ src/main/java/asd/vinted/data/service/impl/CityServiceImpl.java
@@ -1,4 +1,21 @@
 package asd.vinted.data.service.impl;
 
-public class CityServiceImpl {
+import asd.vinted.data.dao.CityDao;
+import asd.vinted.data.entity.City;
+import asd.vinted.data.service.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class CityServiceImpl implements CityService {
+
+    @Autowired
+    CityDao cityDao;
+
+
+    @Override
+    public List<City> findAll() {
+        return  cityDao.findAll();
+    }
 }
