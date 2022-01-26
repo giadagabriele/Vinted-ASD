bfb1b6e0742d5a13a83d4dc1ef437e527a8f1faf
Gebreyowhans <gebreyohans2005@gmail.com>
2022-01-26 23:00:20 +0100

    Saving order details to db in CCD

--- src/main/java/asd/vinted/data/service/impl/CreditCardPaymentServiceImpl.java
+++ src/main/java/asd/vinted/data/service/impl/CreditCardPaymentServiceImpl.java
@@ -4,6 +4,7 @@ import asd.vinted.data.dto.OrderDto;
 import asd.vinted.data.payment.CreditCardConfig;
 import asd.vinted.data.payment.CreditCardPaymentResponse;
 import asd.vinted.data.service.CreditCardPaymentService;
+import asd.vinted.data.service.OrderService;
 import com.stripe.Stripe;
 import com.stripe.model.Charge;
 import com.stripe.param.ChargeCreateParams;
@@ -15,13 +16,15 @@ import org.springframework.stereotype.Service;
 public class CreditCardPaymentServiceImpl implements CreditCardPaymentService {
     @Autowired
     private CreditCardConfig config;
+    @Autowired
+    private OrderService orderService;
 
     @Override
     public CreditCardPaymentResponse CreatCreditCardPayment(OrderDto createPayment) {
 
         CreditCardPaymentResponse response = null;
         try {
-            //Stripe.apiKey = config.getApiKey();
+           // Stripe.apiKey = config.getApiKey();
            Stripe.apiKey = "sk_test_51KJQYrGYh3JcLLFyylP8w7TExdqsgiuPvCHt3LUi5m1ZTpotdGHQZpLLkmddpl1QEOOuE2uXvFkFRfZmE087K7uS00oAYjE0Ef";
 
             ChargeCreateParams params =
@@ -32,12 +35,30 @@ public class CreditCardPaymentServiceImpl implements CreditCardPaymentService {
                             .setSource(createPayment.getStripeToken())
                             .build();
 
-            Charge charge = Charge.create(params);
-            if (charge.getStatus().equals("succeeded")){
+            Charge orderCharge = Charge.create(params);
+            if (orderCharge.getStatus().equals("succeeded")){
                 response = new CreditCardPaymentResponse();
-                response.setStatus(charge.getStatus());
-                response.setAmount(charge.getAmount()/100);
-                response.setPaymentID(charge.getId());
+                response.setStatus(orderCharge.getStatus());
+                response.setAmount(orderCharge.getAmount()/100);
+                response.setPaymentID(orderCharge.getId());
+
+                // Save Order details to DB
+
+                OrderDto orderDto = new OrderDto();
+                orderDto.setPrice(response.getAmount());
+                orderDto.setMethod("CCD");
+                orderDto.setCurrency(orderCharge.getCurrency());
+                orderDto.setDescription(orderCharge.getDescription());
+                orderDto.setIntent(orderCharge.getPaymentIntent());
+               // orderDto.setPaymentID(orderCharge.get);
+//                orderDto.setPayer_id( response.getPayer_id());
+                  orderDto.setPayeeName(orderCharge.getReceiptEmail());
+               orderDto.setPaymentDate(orderCharge.getReceiptEmail());
+               orderDto.setPayerEmail(orderCharge.getBillingDetails().getName());
+//
+                orderService.saveOrderDetails(orderDto);
+
+
                 return response;
             }
 
