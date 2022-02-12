58398f190c7d069b26d7bb7f666f7e2300a5cb2e
Giada Gabriele <giadagabriele@protonmail.com>
2022-02-12 16:16:38 +0100

    Delete categories.sql

--- categories.sql
+++
@@ -1,7 +0,0 @@
-
-INSERT INTO `categories` (`id`,`name`) VALUES (1,'shoes');
-INSERT INTO `categories` (`id`,`name`) VALUES (2,'technology');
-INSERT INTO `categories` (`id`,`name`) VALUES (3,'clothes');
-INSERT INTO `categories` (`id`,`name`) VALUES (4,'books');
-INSERT INTO `categories` (`id`,`name`) VALUES (5,'cook');
-INSERT INTO `categories` (`id`,`name`) VALUES (6,'accessories');
