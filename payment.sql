473733262b31dc383716a07ed77c9a6ecaf80f0b
Giada Gabriele <giadagabriele@protonmail.com>
2022-02-12 16:16:50 +0100

    Delete payment.sql

--- payment.sql
+++
@@ -1,8 +0,0 @@
-/*
--- Query: SELECT * FROM vinted.payment
-LIMIT 0, 1000
-
--- Date: 2022-01-31 17:27
-*/
-INSERT INTO `payment` (`id`,`payment_date`,`address_line1`,`city`,`country_code`,`currency`,`description`,`intent`,`method`,`payee_name`,`payer_email`,`payer_name`,`payer_id`,`paymentid`,`payment_mode`,`postal_code`,`price`,`productid`,`state`,`stripe_email`,`stripe_token`,`userid`) VALUES (309,'2022-01-28',NULL,NULL,NULL,'eur','Payment for order',NULL,'CCD',NULL,'janedoe@email.com',NULL,NULL,NULL,NULL,NULL,70,'1',NULL,NULL,NULL,58);
-INSERT INTO `payment` (`id`,`payment_date`,`address_line1`,`city`,`country_code`,`currency`,`description`,`intent`,`method`,`payee_name`,`payer_email`,`payer_name`,`payer_id`,`paymentid`,`payment_mode`,`postal_code`,`price`,`productid`,`state`,`stripe_email`,`stripe_token`,`userid`) VALUES (312,'2022-01-28',NULL,NULL,NULL,'eur','Payment for order',NULL,'CCD',NULL,'janedoe@email.com',NULL,NULL,NULL,NULL,NULL,1300,'12',NULL,NULL,NULL,58);
